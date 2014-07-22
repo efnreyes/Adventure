2fd23955090fd60328471afdf36a60fc7a05d4ce
Efrén Reyes <efren.reyes@globant.com>
2014-07-22 13:43:21 -0500

    Initial Commit

---
+++ Adventure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventure
+//
+//  Created by Efrén Reyes Torres on 7/22/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Adventure/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Adventure
+//
+//  Created by Efrén Reyes Torres on 7/22/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
